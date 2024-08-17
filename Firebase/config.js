d4c370379d701a269706f1904691cc66e05590bc
Renan <renanvgmartins@hotmail.com>
2024-08-17 13:40:14 -0300

    Variáveis env

--- Firebase/config.js
+++ Firebase/config.js
@@ -1,8 +1,8 @@
 const admin = require('firebase-admin')
-const service_account = require('./serviceAccountKey.json')
+// const service_account = require('./serviceAccountKey.json')
 
 admin.initializeApp({
-    credential: admin.credential.cert(service_account),
+    credential: admin.credential.cert(JSON.parse(process.env.FIREBASE_CONFIG)),
     storageBucket: process.env.STORAGEBUCKET
 })
 
